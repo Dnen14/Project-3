55901209d039cefb19ca2ae89d8cede20d1b8360
Dnen14 <abhinav.nallam@gmail.com>
2023-11-06 19:19:58 -0600

    Added files

---
+++ client/src/pages/Customer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Customer.css';
+
+function Customer() {
+  return (
+    <div>
+      <h2>Customer View</h2>
+      <p>Welcome, customer! Here's your view.</p>
+    </div>
+  );
+}
+
+export default Customer;
